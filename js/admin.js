ad345d67f821c9eba8bb8d55ea6d4bdab1a18c9c
diegofdz <fernandezdiegonahuel@gmail.com>
2021-12-14 10:15:41 -0300

    se agrego la logica para mostrar los datos en la tabla de productos

--- js/admin.js
+++ js/admin.js
@@ -1,5 +1,10 @@
-import {campoRequerido,validarNumeros, validarGeneral,validarURL } from "./validaciones.js"
-import {Producto} from "./productoClass.js";
+import {
+  campoRequerido,
+  validarNumeros,
+  validarGeneral,
+  validarURL,
+} from "./validaciones.js";
+import { Producto } from "./productoClass.js";
 
 // traigo el elemento que necesito del html (imput)
 let campoCodigo = document.querySelector("#codigo");
@@ -8,55 +13,109 @@ let campoDescripcion = document.querySelector("#descripcion");
 let campoCantidad = document.querySelector("#cantidad");
 let campoURL = document.querySelector("#url");
 let formularioProducto = document.querySelector("#formProducto");
-//si hay algo en localstorage quiero guardarlo en el arreglo sino quiero que sea un arreglo vacio 
-let listaProductos = JSON.parse(localStorage.getItem("arreglosProductosKey")) || [];
+//si hay algo en localstorage quiero guardarlo en el arreglo sino quiero que sea un arreglo vacio
+let listaProductos =
+  JSON.parse(localStorage.getItem("arreglosProductosKey")) || [];
 
 // asociar un evento a un elemtento del hyml desde js
 // ddEventListener  : manejador de evento 2 parametros (nombre evento mas funcion asociada)
-campoCodigo.addEventListener("blur", () => {campoRequerido(campoCodigo);});
-campoProducto.addEventListener("blur", () => {campoRequerido(campoProducto);});
-campoDescripcion.addEventListener("blur", () => {campoRequerido(campoDescripcion);});
-campoCantidad.addEventListener("blur", () => {validarNumeros(campoCantidad);});
-campoURL.addEventListener("blur", () => {validarURL(campoURL);});
-formularioProducto.addEventListener("submit", guardarProducto );
+campoCodigo.addEventListener("blur", () => {
+  campoRequerido(campoCodigo);
+});
+campoProducto.addEventListener("blur", () => {
+  campoRequerido(campoProducto);
+});
+campoDescripcion.addEventListener("blur", () => {
+  campoRequerido(campoDescripcion);
+});
+campoCantidad.addEventListener("blur", () => {
+  validarNumeros(campoCantidad);
+});
+campoURL.addEventListener("blur", () => {
+  validarURL(campoURL);
+});
+formularioProducto.addEventListener("submit", guardarProducto);
 
-function guardarProducto(e){
-    //verificar que todos los datos sean validados
-    e.preventDefault();
-    if(validarGeneral(campoCodigo, campoProducto, campoDescripcion, campoCantidad, campoURL)){
+//llamo a carga inicial
+cargaInicial();
+
+function guardarProducto(e) {
+  //verificar que todos los datos sean validados
+  e.preventDefault();
+  if (
+    validarGeneral(
+      campoCodigo,
+      campoProducto,
+      campoDescripcion,
+      campoCantidad,
+      campoURL
+    )
+  ) {
     // crear producto
     crearProducto();
-    }
+  }
 }
 
-function crearProducto(){
-    // crear un objeto producto
-    console.log("aqui tengo que crear el producto")
-    let productoNuevo = new Producto(campoCodigo.value, campoProducto.value, campoDescripcion.value, campoCantidad.value, campoURL.value);
-    // guardar el objeto dentro del arreglo de producto
-    console.log(productoNuevo);
-    listaProductos.push(productoNuevo);
-    console.log(listaProductos);
-    //limpiar el formulario
-    limpiarFormulario();
-    //guardar el arreglo de productos dentro de localstorage
-    guardarLocalStorage();
-    // mostrar un cartel al usuario
- 
+function crearProducto() {
+  // crear un objeto producto
+  console.log("aqui tengo que crear el producto");
+  let productoNuevo = new Producto(
+    campoCodigo.value,
+    campoProducto.value,
+    campoDescripcion.value,
+    campoCantidad.value,
+    campoURL.value
+  );
+  // guardar el objeto dentro del arreglo de producto
+  console.log(productoNuevo);
+  listaProductos.push(productoNuevo);
+  console.log(listaProductos);
+  //limpiar el formulario
+  limpiarFormulario();
+  //guardar el arreglo de productos dentro de localstorage
+  guardarLocalStorage();
+  // mostrar un cartel al usuario
+  Swal.fire(
+    "Producto creado",
+    "Su producto fue correctamente cargado",
+    "success"
+  );
+  //cargar producto en la tabla
+  crearFila(productoNuevo);
 }
 
-function limpiarFormulario(){
-    //limpiamos los value de un formulario
-    formularioProducto.reset();
-    // resetear las clases 
-    campoCodigo.className = "form-control";
-    campoProducto.className = "form-control";
-    campoDescripcion.className = "form-control";
-    campoCantidad.className = "form-control";
-    campoURL.className = "form-control";
-
+function limpiarFormulario() {
+  //limpiamos los value de un formulario
+  formularioProducto.reset();
+  // resetear las clases
+  campoCodigo.className = "form-control";
+  campoProducto.className = "form-control";
+  campoDescripcion.className = "form-control";
+  campoCantidad.className = "form-control";
+  campoURL.className = "form-control";
 }
-function guardarLocalStorage(){
-    localStorage.setItem("arreglosProductosKey", JSON.stringify(listaProductos))
+function guardarLocalStorage() {
+  localStorage.setItem("arreglosProductosKey", JSON.stringify(listaProductos));
+}
 
+function crearFila(producto) {
+  let tablaProductos = document.getElementById("tablaProductos");
+  tablaProductos.innerHTML += `<tr>
+    <th>${producto.codigo}</th>
+    <td>${producto.producto}</td>
+    <td>${producto.descripcion}</td>
+    <td>${producto.cantidad}</td>
+    <td>${producto.url}</td>
+    <td class="text-center"><button class="btn btn-warning" type="submit">Editar</button>
+      <button class="btn btn-danger" type="submit">Borrar</button></td>
+  </tr>`;
+}
+
+function cargaInicial() {
+  if (listaProductos.length > 0) {
+    // crear las final
+    listaProductos.forEach((itemProducto) => {
+      crearFila(itemProducto);
+    });
+  }
 }
--- js/productoClass.js
+++ js/productoClass.js
@@ -2,7 +2,7 @@
     constructor(parametroCodigo,parametroProducto, parametroDescripcion, parametroCantidad, parametroURL){
         this.codigo = parametroCodigo;
         this.producto = parametroProducto;
-        this.desripcion = parametroDescripcion;
+        this.descripcion = parametroDescripcion;
         this.cantidad = parametroCantidad;
         this.url = parametroURL;
     }
